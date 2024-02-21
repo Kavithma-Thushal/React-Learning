705b6753de835c89d2b404c33291bd5dd47649fc
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-21 15:04:02 +0530

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client';
 import React from "react";
+import ReactDOM from 'react-dom/client';
 
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
