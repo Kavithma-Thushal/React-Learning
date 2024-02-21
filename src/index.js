95967526c8409597864af15dcb8290f62a852f2c
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-21 14:51:48 +0530

    index.js Added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import React from "react";
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <h1>This is my first react app</h1>
+    </React.StrictMode>
+)
