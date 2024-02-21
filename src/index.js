97f99d525b6b93fb22aafb62022683e7f33203e8
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-21 14:53:09 +0530

    index.js Refactored

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import React from "react";
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <h1>This is my first react app</h1>
+        <h1>This is My First React App</h1>
     </React.StrictMode>
 )
