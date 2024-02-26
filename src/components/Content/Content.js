99d2467adae3926de71e85ef061131fecf1f58b1
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 20:44:04 +0530

    Customer Management Added

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,21 +4,15 @@ export default function Content() {
     return (
         <div>
             <p id="paragraph">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium adipisci beatae doloremque,
-                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
-                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
-                adipisci beatae doloremque,
-                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
-                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
-                adipisci beatae doloremque,
-                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
-                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
-                adipisci beatae doloremque,
-                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
-                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
-                adipisci beatae doloremque,
-                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
-                voluptas. Excepturi, praesentium!
+                Customer management involves the strategic and operational activities undertaken by businesses to build
+                and maintain relationships with their customers throughout the entire customer lifecycle. This
+                multifaceted process encompasses various aspects such as understanding customer needs and preferences,
+                acquiring new customers, retaining existing ones, providing exceptional customer service, and fostering
+                loyalty. Effective customer management involves utilizing tools like customer relationship management
+                (CRM) software, implementing personalized marketing strategies, actively engaging with customers through
+                multiple channels, and continuously analyzing feedback to improve products and services. By prioritizing
+                customer satisfaction and loyalty, businesses can enhance their reputation, increase customer lifetime
+                value, and ultimately drive sustainable growth.
             </p>
         </div>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,10 @@ import React from "react";
 import './Navigation.css'
 
 export default function Navigation() {
-    let string1 = "React App";
-    let string2 = "NavBar";
-    let string3 = "WEB";
+    let string1 = "Customer Management";
+    let string2 = "Item Management";
+    let string3 = "Order Management";
+
     return (
         <div>
             <div id="navBar">
