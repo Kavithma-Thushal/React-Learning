fda42bb30c8443a7d2cac38ced62227dbd5653aa
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 15:38:00 +0530

    React Component Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PartOne from "./components/PartOne";
 
 function App() {
     return (
         <div>
             <h1>This is My First React App</h1>
+            <PartOne/>
         </div>
     );
 }
---
+++ src/components/PartOne.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PartOne() {
+    return (
+        <div>
+            <h1>This is My First React Component</h1>
+            <h1>This is My First React Component</h1>
+            <h1>This is My First React Component</h1>
+            <h1>This is My First React Component</h1>
+            <h1>This is My First React Component</h1>
+        </div>
+    );
+}
