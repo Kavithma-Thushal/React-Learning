fd74c5425a438fb2360d2be11ef7604eee5ad9c8
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 20:40:39 +0530

    Long Content Added

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -6,6 +6,18 @@ export default function Content() {
             <p id="paragraph">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium adipisci beatae doloremque,
                 doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
+                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
+                adipisci beatae doloremque,
+                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
+                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
+                adipisci beatae doloremque,
+                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
+                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
+                adipisci beatae doloremque,
+                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
+                voluptas. Excepturi, praesentium!Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium
+                adipisci beatae doloremque,
+                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
                 voluptas. Excepturi, praesentium!
             </p>
         </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,10 +2,13 @@ import React from "react";
 import './Navigation.css'
 
 export default function Navigation() {
+    let string1 = "React App";
+    let string2 = "NavBar";
+    let string3 = "WEB";
     return (
         <div>
             <div id="navBar">
-                <h2 id="navText">NavBar</h2>
+                <h2 id="navText">{string1}</h2>
             </div>
         </div>
     );
