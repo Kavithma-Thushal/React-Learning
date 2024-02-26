c225784f00174fd055a5a0587603837afe685a5b
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 22:19:22 +0530

    Refactored

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,14 +2,19 @@ import React from "react";
 import './Navigation.css'
 
 export default function Navigation() {
-    let string1 = "Customer Management";
-    let string2 = "Item Management";
-    let string3 = "Order Management";
+    let logo = "React | ";
+    let customer = "Customer Management";
+    let item = "Item Management";
+    let calculate = 10 + 20;
+    let array = [12, 23, 34, 45, 56];
+    let studentObj = {"name": "Thushal", "age": "23", "address": "Galle"};      //Error
+    let bool = 10 < 20;
+    let log = console.log("Hello");
 
     return (
         <div>
             <div id="navBar">
-                <h2 id="navText">{string1}</h2>
+                <h2 id="navText">{logo + customer}</h2>
             </div>
         </div>
     );
