76c39dece8aac9b8099544c0080b0c9a5622ed43
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 18:28:20 +0530

    Paragraph Added

---
+++ src/components/Content/Content.js
@@ -0,0 +1,13 @@
+import "./Content.css"
+
+export default function Content() {
+    return (
+        <div>
+            <p id="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium adipisci beatae doloremque,
+                doloribus earum esse harum illo inventore magni modi officiis porro possimus tenetur ullam vitae
+                voluptas. Excepturi, praesentium!
+            </p>
+        </div>
+    );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import Navigation from "../components/Navigation/Navigation";
+import Content from "../components/Content/Content";
 
 export default function HomePage() {
     return (
         <div>
             <Navigation/>
+            <Content/>
         </div>
     );
 }
