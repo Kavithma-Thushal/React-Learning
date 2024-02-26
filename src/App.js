e30e5249435f06e93791543e5a6096b6be60dde3
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-26 15:20:14 +0530

    Structure Refactored

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>This is My First React App</h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 
 let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <h1>This is My First React App</h1>
+        <App/>
     </React.StrictMode>
 )
