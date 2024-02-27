36c6dc5af48da4f7a17bc9f6e0b91c09559e7c66
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-27 12:38:15 +0530

    React Router DOM Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
-import Home from "./pages/Home";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
     return (
-        <div>
-            <Home/>
-        </div>
+        <Router>
+            <div>
+                <Switch>
+
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+
+                    <Route path="/AboutMe">
+                        <AboutMe/>
+                    </Route>
+
+                    <Route path="/ContactUs">
+                        <ContactUs/>
+                    </Route>
+
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
